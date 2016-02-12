575e3c23d7673c75e5762da30d6d0d3efc67d3f7
Benjamin Saint-Sever <angelbj6@gmail.com>
2016-02-12 23:37:10 +0100

    main

--- Agenda/main.cpp
+++ Agenda/main.cpp
@@ -7,9 +7,22 @@
 //
 
 #include <iostream>
+#include "Entree.h"
+#include "Tableau.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Entree e;
+    e.editerNom_Num("ben","0611111110");
+    e.afficherEntree();
+    
+    Tableau t(10);
+    t.addEntree("Benji", "0615151515");
+    t.addEntree("alex", "0615161616");
+    t.afficherTableau();
+    t.deleteEntree("alex");
+    t.afficherTableau();
+    
+    cout<< "\nTaille du tableau : " <<t.getSize() << "\nNombre d'elements : " <<t.getNbElement() <<endl;
+    
     return 0;
 }
